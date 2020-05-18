5b7615db3a5dd145e030c0256cb4a598482bd1fb
mfarzi <m.farzi@leeds.ac.uk>
2020-05-18 10:32:04 +0100

    minor edits to myoscopeLoadModel to correct syntax errors

--- matlab_help_functions/myoscopeLoadModel.m
+++ matlab_help_functions/myoscopeLoadModel.m
@@ -21,17 +21,25 @@ config = searchConfig(fileName, 'name', 'constraints', 'params', 'hyperparams');
 model = str2model(config.name);
 
 % make sure links are of the same type
-linkType = textscan(config.constraints{1}, '%s', 'delimiter', {':',','});
-linkType{1} = [];
+tmp = textscan(config.constraints{1}, '%s', 'delimiter', {':',','});
+linkType = tmp{1};
+linkType(1) = [];
 
 numOfLinks = length(model.links);
 for thisLinkNo = 1:numOfLinks
-    if strm(linkType{thisLinkNo}
-    model.links(thisLinkNo).set('type', linkType{thisLinkNo});
+    isDummy = strcmp(linkType{thisLinkNo}, 'dummy');
+    isMatched = strcmp(linkType{thisLinkNo}, model.links(thisLinkNo).type);
+    if ~isMatched && ~isDummy
+        model.links(thisLinkNo).set('type', linkType{thisLinkNo});
+    end
 end
+
+% check for other constraints
 defaultConstraints = model.getConstraints;
 
 idx = ismember(config.constraints, defaultConstraints);
+idx(1) = true; % link types have been checked previously
+
 thisConstraints = config.constraints(~idx);
 % enforce constraints
 nConstraints = length(thisConstraints);
