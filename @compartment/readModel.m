3cd29d79a9e40219d84f78b9aa9cbd7d63d09b67
mfarzi <m.farzi@leeds.ac.uk>
2021-10-12 11:46:58 +0100

    edit readModel to account for the default constraints which have been removed

--- @compartment/readModel.m
+++ @compartment/readModel.m
@@ -15,8 +15,25 @@ function model = readModel(filename)
 config = readConfig(filename, 'model');
 
 %% construct the model
-model = multicompartment.str2model(config.name{1}{1});
+model = compartment.str2model(config.name{1}{1});
 model.setParamsName(config.paramsName{1});
+
+% remove default constraints which are not in the config
+defaultConstraints = model.getConstraints;
+configConstraints = cellfun(@(c) c{1}, config.constraints,...
+    'UniformOutput', false);
+nDeafultConstraints = length(defaultConstraints);
+for i=1:nDeafultConstraints
+    if not(ismember(defaultConstraints{i},configConstraints))
+        % find the left-hand variable
+        str = defaultConstraints{i};
+        lhv = cellfun(@(c) startsWith(str, c), config.paramsName{1});
+        % remove the active constraint
+        if ~strcmp(model.links(lhv).type, 'independent')
+            model.removeConstraint(config.paramsName{1}{lhv});
+        end
+    end
+end
 
 % enforce new constraints [not set by deafult]
 nConstraints = length(config.constraints);
