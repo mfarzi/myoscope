5072bed29fe66290a742d187692e5917f39347f4
mfarzi <m.farzi@leeds.ac.uk>
2022-03-10 15:17:35 +0000

    edit initx such that random parameters are selected between lower and upper bounds from a uniform distribution.

--- @linker/initx.m
+++ @linker/initx.m
@@ -1,5 +1,18 @@
 function x = initx(obj)
     % initx is a public method to initialise x
-    N = obj.getVarNum('free');
-    x = rand(N,1)*pi/2; 
+    N = obj.getVarNum();
+    x = zeros(N, 1);
+    for n=1:N
+        if obj(n).bounded
+            x(n) = acos(sqrt(rand(1)));
+        else
+            x(n) = rand(1)*2*pi;
+        end
+    end
+    
+    % remove dummy or constant variables
+    isDummy = strcmp({obj.type}', 'dummy');
+    isConstant = strcmp({obj.type}', 'constant');
+    x(isConstant|isDummy) = [];
+    %x = rand(N,1)*pi/2; 
 end
